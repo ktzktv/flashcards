c4186ebf7b5c1e9bc435485d0cf4cb994d1324de
Steve <kutzko.g@husky.neu.edu>
2020-04-11 11:31:27 -0400

    Updates

---
+++ precache-manifest.69cd8060ddf9e0a717929a233c5f03ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cc38adedca4322cde587e1f9842190",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "3f81293eb89c808d17e2",
+    "url": "/flashcards/static/css/main.4d08c02a.chunk.css"
+  },
+  {
+    "revision": "13947d56abb0e3659087",
+    "url": "/flashcards/static/js/2.bc9cbbe4.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/flashcards/static/js/2.bc9cbbe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f81293eb89c808d17e2",
+    "url": "/flashcards/static/js/main.690d838f.chunk.js"
+  },
+  {
+    "revision": "7893ea715fe78f0a4b91",
+    "url": "/flashcards/static/js/runtime-main.8f5b6e37.js"
+  }
+]);
